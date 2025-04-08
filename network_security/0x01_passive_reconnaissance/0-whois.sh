a152d9e1dfe7ff5fd55438cbaf8ca6d39077c60f
Diogo <diogoanonimos@gmail.com>
2025-04-08 14:59:15 +0200

    0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F ': +' 'BEGIN{OFS=","}/^Registrant |^Admin |^Tech /{gsub(/ /,",",$1);print $1,$2}' > "${1}.csv"
+whois "$1" | awk -F ': +' 'BEGIN{OFS=","}/^Registrant |^Admin |^Tech /{print $1,$2}' > "${1}.csv"
