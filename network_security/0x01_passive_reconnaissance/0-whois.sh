0d3b685c9eb47f571eae87ff9e42c20c1251de2d
Diogo <diogoanonimos@gmail.com>
2025-04-08 14:56:39 +0200

    0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-domain="$1"
-output_file="${domain}.csv"
-
-whois "$domain" | awk -F ': +' '
-    BEGIN { OFS = "," }
-    /^Registrant / || /^Admin / || /^Tech / {
-        gsub(/ /, ",", $1)  # Replace spaces in field names with commas
-        print $1, $2
-    }
-' > "$output_file"
+whois "$1" | awk -F ': +' 'BEGIN{OFS=","}/^Registrant |^Admin |^Tech /{gsub(/ /,",",$1);print $1,$2}' > "${1}.csv"
