13470ccb632d6a7c4285c1942fb6c2cbcf5a7e11
Diogo <diogoanonimos@gmail.com>
2025-05-08 16:59:05 +0200

    1-xor_decoder.sh

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-decoded=$(echo -n "${1#\{xor\}}" | base64 -d)
-
+password="$1"
+password="${password#'{xor}'}"
+decoded_password=$(echo -n "$password" | openssl enc -base64 -d)
 output=""
-for (( i=0; i<${#decoded}; i++ )); do
-  char="${decoded:i:1}"
-  xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
-  output+=$(printf "\\$(printf '%03o' "$xor_result")")
+
+for ((i = 0; i < ${#decoded_password}; i++)); do
+    char="${decoded_password:$i:1}"
+    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
+    output+=$(printf "\\$(printf '%03o' $xor_result)")
 done
 
 echo "$output"
