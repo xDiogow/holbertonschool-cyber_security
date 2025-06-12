34c5118d86ffeb5038a5cc68a8199c208ac28dac
Diogo <diogoanonimos@gmail.com>
2025-06-12 19:14:11 +0200

    5-windows_john.sh

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1"
-john --show --format=nt "$1" | cut -d':' -f2 | grep -v '^$' > 5-password.txt
+#!/bin/bash
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" && john --show --format=nt "$1" | cut -d':' -f2 | grep -v '^$' > 5-password.txt
