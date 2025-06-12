748f807ba858eb75dba16eb7f67594343aaeada8
Diogo <diogoanonimos@gmail.com>
2025-06-12 19:24:36 +0200

    full

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 "$1" && john --show --format=raw-sha256 "$1" | cut -d':' -f2 | grep -v '^$' > 6-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-SHA256 $1
