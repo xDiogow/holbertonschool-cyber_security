3e42a3b7d70f4187810d2d58d088e41e1da03f7b
Diogo <diogoanonimos@gmail.com>
2025-07-21 07:02:37 +0200

    3-library.sh

---
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+attacker_ip=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+grep "^$attacker_ip" logs.txt | awk -F'"' '{print $(NF-2)}' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print $0}' | sed 's/^ *//'
