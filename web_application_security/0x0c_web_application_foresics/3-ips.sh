ec0fb1b215d82fbc669557e10c8935e85bd9013c
Diogo <diogoanonimos@gmail.com>
2025-07-28 09:55:11 +0200

    3-ips.sh

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-grep "Accepted password" auth.log | awk '{print $11}' | sort -u | wc -l
+awk '
+/Failed password/ {
+  for (i = 1; i < NF; i++) if ($i == "from") fail[$(i+1)]++
+}
+/Accepted password/ {
+  for (i = 1; i < NF; i++) if ($i == "from") success[$(i+1)]++
+}
+END {
+  for (ip in success) {
+    if (fail[ip]) {
+      count++
+    }
+  }
+  print count
+}' auth.log
