598e68947eabb510cb5e252e962a3e75d075f8fa
Diogo <diogoanonimos@gmail.com>
2025-07-28 09:45:48 +0200

    2-accounts.sh

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-tac auth.log | head -n 1000 | awk '/Failed password/ {fail[$NF]++} /Accepted password/ {success[$NF]++}
-END {
-  for (user in success) {
-    if (fail[user] > 5) print user;
-  }
-}' | sort | uniq
-
+grep "Accepted password" auth.log | awk '{print $9}' | sort | uniq
