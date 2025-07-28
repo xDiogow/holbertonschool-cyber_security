4dbdc5c498dad376d783196d8076a628c72a1f7e
Diogo <diogoanonimos@gmail.com>
2025-07-28 09:47:26 +0200

    2-accounts.sh

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-grep "Accepted password" auth.log | awk '{print $9}' | sort | uniq
+awk '
+/Failed password/ {fail[$9]++}
+/Accepted password/ {success[$9]++}
+END {
+  max = 0
+  user = ""
+  for (u in success) {
+    if (fail[u] > max) {
+      max = fail[u]
+      user = u
+    }
+  }
+  if (user != "") print user
+}' auth.log
+
