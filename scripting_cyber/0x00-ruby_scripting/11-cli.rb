47ba5d5b247aeabe5187f13f824d0f453f5ff571
Diogo <diogoanonimos@gmail.com>
2025-08-25 10:54:36 +0200

    ruby

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -1,43 +1,83 @@
-#!/usr/bin/ruby
-
-
+#!/usr/bin/env ruby
 require 'optparse'
 
+# Load tasks from the file
+def load_tasks
+  if File.exist?('tasks.txt')
+    File.readlines('tasks.txt').map(&:chomp)
+  else
+    []
+  end
+end
+
+# Save tasks to the file
+def save_tasks(tasks)
+  File.open('tasks.txt', 'w') do |file|
+    tasks.each { |task| file.puts(task) }
+  end
+end
+
+# Add a new task
+def add_task(task)
+  tasks = load_tasks
+  tasks << task
+  save_tasks(tasks)
+  puts "Task '#{task}' added."
+end
+
+# List all tasks
+def list_tasks
+  tasks = load_tasks
+  if tasks.empty?
+    puts "No tasks available."
+  else
+    puts "Tasks:"
+    tasks.each_with_index { |task, index| puts "#{index + 1}. #{task}" }
+  end
+end
+
+# Remove a task by index
+def remove_task(index)
+  tasks = load_tasks
+  if index.between?(1, tasks.length)
+    removed_task = tasks.delete_at(index - 1)
+    save_tasks(tasks)
+    puts "Task '#{removed_task}' removed."
+  else
+    puts "Invalid task index."
+  end
+end
+
+# Command-line options and argument parsing
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: 11-cli.rb [options]"
+  opts.banner = "Usage: cli.rb [options]"
 
-  opts.on("-a", "--add TASK", "Add a new task") do |v|
-    options[:add] = v
+  opts.on("-a", "--add TASK", "Add a new task") do |task|
+    options[:add] = task
   end
 
-  opts.on("-l", "--list", "List all tasks") do |v|
-    options[:list] = v
+  opts.on("-l", "--list", "List all tasks") do
+    options[:list] = true
   end
 
-  opts.on("-r", "--remove INDEX", "Remove a task by index") do |v|
-    options[:remove] = v
+  opts.on("-r", "--remove INDEX", "Remove a task by index") do |index|
+    options[:remove] = index.to_i
   end
 
-  opts.on("-h", "--help", "Show help") do |v|
+  opts.on("-h", "--help", "Show help") do
     puts opts
     exit
   end
 end.parse!
 
+# Process options
 if options[:add]
-  puts "Task '#{options[:add]}' added."
-  puts 'Usage: 11-cli [options]'
-end
-
-if options[:remove]
-  puts "Task 'Task2' removed."
-  puts 'Usage: 11-cli [options]'
-end
-
-if options[:list]
-  puts 'Tasks:'
-  puts "1. Task1"
-  puts "2. Task2"
-  puts 'Usage: 11-cli [options]'
+  add_task(options[:add])
+elsif options[:list]
+  list_tasks
+elsif options[:remove]
+  remove_task(options[:remove])
+else
+  puts "Usage: cli.rb [options]"
 end
